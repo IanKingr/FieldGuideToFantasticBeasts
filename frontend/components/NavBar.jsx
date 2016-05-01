3d78d300611396887a8c53bb2ca21069fb2c4769
Ian <iankingr@gmail.com>
2016-04-30 20:37:49 -0700

    Refactored NavBar and SignUp/SignIn

---
+++ frontend/components/NavBar.jsx
@@ -0,0 +1,131 @@
+var React = require('react');
+var Signup = require('./Signup');
+var Signin = require('./Signin');
+var UserActions = require('../actions/userActions');
+var Buttons = require('./buttons');
+var UserStore = require('../stores/user_store');
+var Modal = require("react-modal");
+
+
+var style = {
+  overlay : {
+    position        : 'fixed',
+    top             : 0,
+    left            : 0,
+    right           : 0,
+    bottom          : 0,
+    backgroundColor : 'rgba(255, 255, 255, 0.75)',
+    zIndex         : 10
+  },
+  content : {
+    position        : 'fixed',
+    top             : '100px',
+    left            : '150px',
+    right           : '150px',
+    bottom          : '100px',
+    backgroundColor : '#FFF2E7',
+    border          : '1px solid #ccc',
+    padding         : '20px',
+    zIndex         : 11
+  }
+};
+
+UserActions.fetchCurrentUser();
+
+var NavBar = React.createClass({
+
+  componentDidMount: function(){
+    this.userListener = UserStore.addListener(this.getCurrentUser);
+  },
+
+  componentWillUnmount: function(){
+    this.userListener.remove();
+  },
+  // componentWillUnmount: function(){
+  //   this.beastListener.remove();
+  // },
+
+  // getCurrentBeast: function(){
+
+  //   this.setState({beast: BeastStore.currentBeast()});
+  // },
+
+  getInitialState: function(){
+    return {
+      SignUpModalOpen: false,
+      SignInModalOpen: false,
+      currentUser: null,
+    };
+  },
+
+  handleSignin: function(){
+    console.log("Sign In Clicked");
+    this.setState({SignInModalOpen: true});
+  },
+
+  handleSignup: function(){
+    console.log("Sign Up Clicked");
+    this.setState({SignUpModalOpen: true});
+  },
+
+  handleSignout: function(){
+    console.log("Sign out Clicked");
+    UserActions.logout();
+  },
+
+
+  getCurrentUser: function(){
+    this.setState({currentUser: UserStore.currentUser()});
+    if(this.state.currentUser){
+      this.closeModal(); 
+      console.log("Current User is " + this.state.currentUser.username);
+    } else {
+      console.log("getCurrentUser = none");
+      this.setState({errors: UserStore.errors});
+    }
+  },
+
+
+  closeModal: function(){
+    console.log("Closing User Modal in FieldGuideIndex");
+    this.setState({
+      SignUpModalOpen: false,
+      SignInModalOpen: false,
+    });
+  },
+
+  render: function(){
+    var username = this.state.currentUser ? this.state.currentUser.username : null;
+
+    return (
+      <div className="BeastContainer">
+        <div className="nav"><img src="http://res.cloudinary.com/flyingonclouds/image/upload/v1461973955/field_guide_logo3_tojbyd.png"/>
+
+        <Buttons
+          signed_in={this.state.currentUser}
+          signOutCallback={this.handleSignout}
+          signUpCallback={this.handleSignup}
+          signInCallback={this.handleSignin}
+          username={username}/>
+        </div>
+
+        <Modal
+          isOpen={this.state.SignUpModalOpen}
+          onRequestClose={this.closeModal}
+          style={style}>
+          <Signup />
+        </Modal>
+
+        <Modal
+          isOpen={this.state.SignInModalOpen}
+          onRequestClose={this.closeModal}
+          style={style}>
+          <Signin />
+        </Modal>
+
+      </div>
+    );
+  }
+});
+
+module.exports = NavBar;
--- frontend/field_guide.jsx
+++ frontend/field_guide.jsx
@@ -1,17 +1,24 @@
 var React = require('react'),
     ReactDOM = require('react-dom'),
     Modal = require("react-modal"),
-    Signup = require('./components/Signup'),
-    Signin = require('./components/Signin'),
-    UserActions = require('./actions/userActions'),
-    Buttons = require('./components/buttons'),
-    UserStore = require('./stores/user_store'),
+    // Signup = require('./components/Signup'),
+    // Signin = require('./components/Signin'),
+    // UserActions = require('./actions/userActions'),
+    // Buttons = require('./components/buttons'),
+    // UserStore = require('./stores/user_store'),
     BeastForm = require('./components/BeastForm'),
     Beast = require('./components/Beast'),
     BeastActions = require('./actions/beastActions'),
     BeastStore = require('./stores/beast_store'),
     BeastIndex = require('./components/BeastIndex'),
-    AffinityBeastList = require('./components/AffinityBeastList');
+    AffinityBeastList = require('./components/AffinityBeastList'),
+    NavBar = require('./components/NavBar');
+
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+var Link = require('react-router').Link;
+var BrowserHistory = require('react-router').browserHistory;
+
 
 window.BeastActions = BeastActions;
 window.BeastStore = BeastStore;
@@ -39,15 +46,15 @@ var style = {
   }
 };
 
-UserActions.fetchCurrentUser();
+// UserActions.fetchCurrentUser();
 
 var FieldGuide = React.createClass({
   getInitialState: function(){
     return({
-      SignUpModalOpen: false,
-      SignInModalOpen: false,
+      // SignUpModalOpen: false,
+      // SignInModalOpen: false,
       CreateBeastModalOpen: false,
-      currentUser: null,
+      // currentUser: null,
       currentBeast:{author_id: 1,
         name: "Mountain Troll",
         description: "A Troll is a magical creature of prodigious strength and little intelligence - a trait which giants seem to have more of.",
@@ -57,50 +64,51 @@ var FieldGuide = React.createClass({
       }
     });
   },
-
-  closeModal: function(){
-    console.log("Closing User Modal in FieldGuideIndex");
-    this.setState({
-      SignUpModalOpen: false,
-      SignInModalOpen: false,
-    });
-  },
+  //
+  // closeModal: function(){
+  //   console.log("Closing User Modal in FieldGuideIndex");
+  //   this.setState({
+  //     SignUpModalOpen: false,
+  //     SignInModalOpen: false,
+  //   });
+  // },
 
   closeBeastModal: function(){
+    console.log("Closing Beast Modal");
     this.setState({
       CreateBeastModalOpen: false
     });
   },
 
-  handleSignin: function(){
-    console.log("Sign In Clicked");
-    this.setState({SignInModalOpen: true});
-  },
+  // handleSignin: function(){
+  //   console.log("Sign In Clicked");
+  //   this.setState({SignInModalOpen: true});
+  // },
+  //
+  // handleSignup: function(){
+  //   console.log("Sign Up Clicked");
+  //   this.setState({SignUpModalOpen: true});
+  // },
 
-  handleSignup: function(){
-    console.log("Sign Up Clicked");
-    this.setState({SignUpModalOpen: true});
-  },
-
-  handleSignout: function(){
-    console.log("Sign out Clicked");
-    UserActions.logout();
-  },
+  // handleSignout: function(){
+  //   console.log("Sign out Clicked");
+  //   UserActions.logout();
+  // },
 
   createBeast: function(){
     this.setState({CreateBeastModalOpen: true});
   },
-
-  getCurrentUser: function(){
-    this.setState({currentUser: UserStore.currentUser()});
-    if(this.state.currentUser){
-      // this.closeModal(); //
-      console.log("Current User is " + this.state.currentUser.username);
-    } else {
-      console.log("getCurrentUser = none");
-      this.setState({errors: UserStore.errors});
-    }
-  },
+  //
+  // getCurrentUser: function(){
+  //   this.setState({currentUser: UserStore.currentUser()});
+  //   if(this.state.currentUser){
+  //     // this.closeModal(); //
+  //     console.log("Current User is " + this.state.currentUser.username);
+  //   } else {
+  //     console.log("getCurrentUser = none");
+  //     this.setState({errors: UserStore.errors});
+  //   }
+  // },
 
   getMountainAffinity: function(){
     BeastActions.fetchBeasts({affinity_id: 1});
@@ -117,29 +125,21 @@ var FieldGuide = React.createClass({
   },
 
   componentDidMount: function(){
-    this.userListener = UserStore.addListener(this.getCurrentUser);
+
     this.beastListener = BeastStore.addListener(this.getBeasts);
   },
 
   componentWillUnmount: function(){
-    this.userListener.remove();
+    // this.userListener.remove();
     this.beastListener.remove();
   },
 
   render: function () {
-    var username = this.state.currentUser ? this.state.currentUser.username : null;
+    // var username = this.state.currentUser ? this.state.currentUser.username : null;
     return (
       <div>
 
-        <div className="nav"><img src="http://res.cloudinary.com/flyingonclouds/image/upload/v1461973955/field_guide_logo3_tojbyd.png"/>
-
-        <Buttons
-          signed_in={this.state.currentUser}
-          signOutCallback={this.handleSignout}
-          signUpCallback={this.handleSignup}
-          signInCallback={this.handleSignin}
-          username={username}/>
-        </div>
+        <NavBar />
         <br />
 
         <button onClick={this.createBeast}>Create Beast</button>
@@ -152,19 +152,6 @@ var FieldGuide = React.createClass({
 
           <BeastIndex beast={this.state.currentBeast} />
 
-        <Modal
-          isOpen={this.state.SignUpModalOpen}
-          onRequestClose={this.closeModal}
-          style={style}>
-          <Signup />
-        </Modal>
-
-        <Modal
-          isOpen={this.state.SignInModalOpen}
-          onRequestClose={this.closeModal}
-          style={style}>
-          <Signin />
-        </Modal>
 
         <Modal
           isOpen={this.state.CreateBeastModalOpen}
